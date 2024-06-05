b29f162cf49855dc4f5792457bdac5b91725d244
Doni <donizetti.souza9@gmail.com>
2024-06-05 18:45:53 -0300

    -cart component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NgxStarRatingModule } from 'ngx-star-rating';
 import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { TagsComponent } from './components/partials/tags/tags.component';
     HomeComponent,
     SearchComponent,
     FoodPageComponent,
-    TagsComponent
+    TagsComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,5 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/service/cart.service';
 import { FoodService } from 'src/app/service/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -12,7 +14,8 @@ export class FoodPageComponent implements OnInit {
 
   food!: Food;
 
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(activatedRoute: ActivatedRoute, foodService: FoodService, 
+    private CartService: CartService, private router: Router) {
     activatedRoute.params.subscribe((params) => {
       if(params.id)
         this.food = foodService.getFoodById(params.id)
@@ -20,6 +23,11 @@ export class FoodPageComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  addToCart(): void {
+    this.CartService.addToCart(this.food)
+    this.router.navigateByUrl('/cart-page')
   }
 
 }
