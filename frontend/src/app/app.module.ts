d25eef32f1c4dfa39a4259102af53307cc44b999
Doni <donizetti.souza9@gmail.com>
2024-06-05 13:45:43 -0300

    -update food details

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NgxStarRatingModule } from 'ngx-star-rating';
 import { SearchComponent } from './components/partials/search/search.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { SearchComponent } from './components/partials/search/search.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    FoodPageComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/service/food.service.ts
+++ frontend/src/app/service/food.service.ts
@@ -24,4 +24,8 @@ export class FoodService {
       }
     });
   }
+
+  getFoodById(foodId:string): Food{
+    return this.getAll().find(food => food.id == foodId) ?? new Food();
+  }
 }
